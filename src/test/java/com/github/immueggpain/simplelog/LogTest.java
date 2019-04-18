7c752825937b5efc93ad7fb13dd6628eef5ae609
Immueggpain <Immueggpain@users.noreply.github.com>
2019-04-18 18:26:54 +0800

    Create LogTest.java

---
+++ src/test/java/com/github/immueggpain/simplelog/LogTest.java
@@ -0,0 +1,14 @@
+package com.github.immueggpain.simplelog;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class LogTest {
+
+	@Test
+	public void testLog() {
+		
+	}
+
+}
