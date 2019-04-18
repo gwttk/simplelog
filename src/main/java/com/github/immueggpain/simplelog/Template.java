80a8f5a2b112da68a5b424a75d386ed329bd8649
Immueggpain <Immueggpain@users.noreply.github.com>
2019-04-18 18:05:17 +0800

    init

---
+++ src/main/java/com/github/immueggpain/simplelog/Template.java
@@ -0,0 +1,16 @@
+package com.github.immueggpain.simplelog;
+
+public class Template {
+
+	public static void main(String[] args) {
+		try {
+			new Template().run(args);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void run(String[] args) {
+	}
+
+}
