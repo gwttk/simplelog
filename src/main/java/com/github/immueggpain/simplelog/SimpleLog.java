6d7ea8dd6ae0c39ab67f46a1ed22bb04fcb88c38
Immueggpain <Immueggpain@users.noreply.github.com>
2019-04-27 16:23:18 +0800

    print exception

--- src/main/java/com/github/immueggpain/simplelog/SimpleLog.java
+++ src/main/java/com/github/immueggpain/simplelog/SimpleLog.java
@@ -9,9 +9,22 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Set;
 
 public class SimpleLog {
 
+	/** Caption for labeling causative exception stack traces */
+	private static final String CAUSE_CAPTION = "Caused by: ";
+
+	/** Caption for labeling suppressed exception stack traces */
+	private static final String SUPPRESSED_CAPTION = "Suppressed: ";
+
+	/** TAB char '\t' converted to spaces */
+	private static final String TAB = "    ";
+
+	private static final Object mutex = new Object();
 	private static ZoneId zoneId = ZoneId.systemDefault();
 	private static DateTimeFormatter dtfmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'~'HH:mm:ss.SSSxxxxx'['VV']'");
 	private static PrintWriter printer;
@@ -20,8 +33,10 @@ public class SimpleLog {
 
 	static {
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			if (printer != null)
-				printer.close();
+			synchronized (mutex) {
+				if (printer != null)
+					printer.close();
+			}
 		}, "SimpleLog-shutdown"));
 	}
 
@@ -43,17 +58,19 @@ public class SimpleLog {
 				break;
 			}
 			String datetime = ZonedDateTime.now(zoneId).format(dtfmt);
-
 			String segName = LocalDateTime.now(zoneId).format(segfmt);
-			if (!segName.equals(fileName)) {
-				if (printer != null)
-					printer.close();
-				printer = new PrintWriter(new BufferedWriter(
-						new OutputStreamWriter(new FileOutputStream(segName + ".log", true), StandardCharsets.UTF_8)));
-				fileName = segName;
-			}
 
-			printer.println(lvl + " " + datetime + " " + line);
+			synchronized (mutex) {
+				if (!segName.equals(fileName)) {
+					if (printer != null)
+						printer.close();
+					printer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+							new FileOutputStream(segName + ".log", true), StandardCharsets.UTF_8)));
+					fileName = segName;
+				}
+
+				printer.println(lvl + " " + datetime + " " + line);
+			}
 		} catch (Throwable e) {
 			// cause this is log, we can't do anything but print console
 			e.printStackTrace();
@@ -66,6 +83,65 @@ public class SimpleLog {
 
 	public static void println(String line, Object... args) {
 		println(0, line, args);
+	}
+
+	public static void printex(Throwable e) {
+		// Guard against malicious overrides of Throwable.equals by
+		// using a Set with identity equality semantics.
+		Set<Throwable> dejaVu = Collections.newSetFromMap(new IdentityHashMap<Throwable, Boolean>());
+		dejaVu.add(e);
+
+		synchronized (mutex) {
+			// Print our stack trace
+			println(String.valueOf(e));
+			StackTraceElement[] trace = e.getStackTrace();
+			for (StackTraceElement traceElement : trace)
+				println("\tat " + traceElement);
+
+			// Print suppressed exceptions, if any
+			for (Throwable se : e.getSuppressed())
+				printEnclosedStackTrace(se, trace, SUPPRESSED_CAPTION, "\t", dejaVu);
+
+			// Print cause, if any
+			Throwable ourCause = e.getCause();
+			if (ourCause != null)
+				printEnclosedStackTrace(ourCause, trace, CAUSE_CAPTION, "", dejaVu);
+		}
+	}
+
+	private static void printEnclosedStackTrace(Throwable e, StackTraceElement[] enclosingTrace, String caption,
+			String prefix, Set<Throwable> dejaVu) {
+		assert Thread.holdsLock(mutex);
+		if (dejaVu.contains(e)) {
+			println(TAB + "[CIRCULAR REFERENCE:" + e + "]");
+		} else {
+			dejaVu.add(e);
+			// Compute number of frames in common between this and enclosing trace
+			StackTraceElement[] trace = e.getStackTrace();
+			int m = trace.length - 1;
+			int n = enclosingTrace.length - 1;
+			while (m >= 0 && n >= 0 && trace[m].equals(enclosingTrace[n])) {
+				m--;
+				n--;
+			}
+			int framesInCommon = trace.length - 1 - m;
+
+			// Print our stack trace
+			println(prefix + caption + e);
+			for (int i = 0; i <= m; i++)
+				println(prefix + TAB + "at " + trace[i]);
+			if (framesInCommon != 0)
+				println(prefix + TAB + "... " + framesInCommon + " more");
+
+			// Print suppressed exceptions, if any
+			for (Throwable se : e.getSuppressed())
+				printEnclosedStackTrace(se, trace, SUPPRESSED_CAPTION, prefix + TAB, dejaVu);
+
+			// Print cause, if any
+			Throwable ourCause = e.getCause();
+			if (ourCause != null)
+				printEnclosedStackTrace(ourCause, trace, CAUSE_CAPTION, prefix, dejaVu);
+		}
 	}
 
 	public static ZoneId getZoneId() {
--- src/test/java/com/github/immueggpain/simplelog/LogTest.java
+++ src/test/java/com/github/immueggpain/simplelog/LogTest.java
@@ -1,7 +1,6 @@
 package com.github.immueggpain.simplelog;
 
-import static com.github.immueggpain.simplelog.SimpleLog.println;
-import com.github.immueggpain.simplelog.SimpleLog;
+import static com.github.immueggpain.simplelog.SimpleLog.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +8,26 @@ public class LogTest {
 
 	@Test
 	public void testLog() {
-		SimpleLog.getPrinter();
+		// level line
 		println(0, "a simple line");
+		// level fmt line
 		println(0, "fmt line %d %s", 233, "ready");
+		// fmt line
+		println("fmt line %d %s", 244, "default");
+		// print exception stack trace
+		printex(new Throwable("excp msg", new Throwable("cause excp")));
+		// print complex exception
+		Throwable cause = new Throwable("cause");
+		Throwable sup1 = new Throwable("sup1");
+		Throwable sup2 = new Throwable("sup2");
+		Throwable outter = new Throwable("outter");
+		outter.initCause(cause);
+		cause.addSuppressed(sup1);
+		outter.addSuppressed(sup1);
+		outter.addSuppressed(sup2);
+		outter.addSuppressed(cause);
+		printex(outter);
+		outter.printStackTrace();
 	}
 
 }
